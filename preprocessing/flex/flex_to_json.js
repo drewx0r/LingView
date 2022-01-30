92deb0028a86ef5f0c80d811e5f854c5e8174d59
Andrew Abdalian <drewx0r@gmail.com>
2022-01-29 23:50:11 -0600

    try to get prefixes to not have both a dash and a plus sign

--- preprocessing/flex/flex_to_json.js
+++ preprocessing/flex/flex_to_json.js
@@ -87,7 +87,7 @@ function concatMorphs(morphsThisTier, wordStartSlot, wordEndSlot) {
     if (maybeAddCompoundSeparator && !isSeparator(nextValue.substring(0, 1))) {
       wordMorphsText += '+';
     }
-    if (!isSeparator(nextValue.substring(-1))) {
+    if (!isSeparator(nextValue.substring(nextValue.length - 1))) {
       maybeAddCompoundSeparator = true;
     }
 
