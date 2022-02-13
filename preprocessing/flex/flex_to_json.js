17584396e068fe655c5dd811a343be52dd21ad9e
Andrew Abdalian <drewx0r@gmail.com>
2022-02-12 20:59:23 -0600

    fix trailing dash detection
    replace substring with slice

--- preprocessing/flex/flex_to_json.js
+++ preprocessing/flex/flex_to_json.js
@@ -88,7 +88,7 @@ function concatMorphs(morphsThisTier, wordStartSlot, wordEndSlot) {
     if (maybeAddCompoundSeparator && !isSeparator(nextValue.substring(0, 1))) {
       wordMorphsText += '+';
     }
-    if (!isSeparator(nextValue.substring(nextValue.length - 1))) {
+    if (!isSeparator(nextValue.slice(-1))) {
       maybeAddCompoundSeparator = true;
     } else {
 	    maybeAddCompoundSeparator = false;
